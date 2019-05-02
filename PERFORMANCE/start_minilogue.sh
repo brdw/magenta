d95068f6ddca8a32222669eeddd13fb797680506
Brad Willard <bwillard@squarespace.com>
2019-05-02 12:43:10 -0400

    changed models

--- PERFORMANCE/start_minilogue.sh
+++ PERFORMANCE/start_minilogue.sh
@@ -1,10 +1,11 @@
 export CLOCK_CONTROL_NUMBER=127
 export MODEL_HOME="/Users/bwillard/code/phantom"
-export MODEL="attention_rnn.mag"
+export MODEL="pianoroll_rnn_nade.mag"
 
 magenta_midi \
 	--input_ports="magenta_clock,minilogue KBD/KNOB" \
 	--output_ports="minilogue SOUND" \
 	--bundle_files="$MODEL_HOME/$MODEL"  \
 	--clock_control_number=$CLOCK_CONTROL_NUMBER \
+	--fixed_temperature=0.50 \
 	--log="DEBUG"
--- PERFORMANCE/start_moog.sh
+++ PERFORMANCE/start_moog.sh
@@ -7,4 +7,5 @@ magenta_midi   \
 	--output_ports="Moog Grandmother"   \
 	--bundle_files="$MODEL_HOME/$MODEL"  \
 	--clock_control_number=$CLOCK_CONTROL_NUMBER \
+	--fixed_temperature 0.60 \
 	--log="DEBUG"
