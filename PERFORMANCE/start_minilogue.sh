ccf5c762728374d3500a9fa433399df91d33ddab
Brad Willard <bwillard@squarespace.com>
2019-05-02 14:36:00 -0400

    changed tuning params

--- PERFORMANCE/start_minilogue.sh
+++ PERFORMANCE/start_minilogue.sh
@@ -1,12 +1,12 @@
 export CLOCK_CONTROL_NUMBER=127
 export MODEL_HOME="/Users/bwillard/code/phantom"
-export MODEL="pianoroll_rnn_nade.mag"
+export MODEL="attention_rnn.mag"
 
 magenta_midi \
 	--input_ports="magenta_clock,minilogue KBD/KNOB" \
 	--output_ports="minilogue SOUND" \
 	--bundle_files="$MODEL_HOME/$MODEL"  \
 	--clock_control_number=$CLOCK_CONTROL_NUMBER \
-	--fixed_temperature=0.50 \
+	--fixed_temperature=1.80 \
 	--response_length=8 \
 	--log="DEBUG"
